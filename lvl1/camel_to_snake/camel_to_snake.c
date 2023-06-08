275cce9fe6b3aec9ae3b72073c200d29a5fbf7fe
palucena <palucena@student.42malaga.com>
2023-06-08 17:51:18 +0200

    Added camel_to_snake

---
+++ lvl1/camel_to_snake/camel_to_snake.c
@@ -0,0 +1,48 @@
+#include <unistd.h>
+#include <stdlib.h>
+
+#include <stdio.h>
+
+void	camel_to_snake(char *camel)
+{
+	int		i;
+	int		j;
+	char	*snake;	
+
+	i = 0;
+	j = 0;
+	while (camel[j])
+	{
+		if (camel[j] >= 'A' && camel[j] <= 'Z')
+			i++;
+		i++;
+		j++;
+	}
+	snake = malloc((i + 1) * sizeof(char));
+	i = 0;
+	j = 0;
+	while (camel[i] != '\0')
+	{
+		if (camel[i] >= 'A' && camel[i] <= 'Z')
+		{
+			snake[j] = '_';
+			snake[++j] = camel[i] + 32;
+		}
+		else
+			snake[j] = camel[i];
+		i++;
+		j++;
+	}
+	snake[j] = '\0';
+	j = -1;
+	while (snake[++j])
+		write(1, &snake[j], 1);
+}
+
+int	main(int ac, char **av)
+{
+	if (ac == 2)
+		camel_to_snake(av[1]);
+	write(1, "\n", 1);
+	return (0);
+}
