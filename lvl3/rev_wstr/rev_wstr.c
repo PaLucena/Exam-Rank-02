d891573bf63d64eb9385d684fe9b838f312d9136
palucena <palucena@student.42malaga.com>
2023-08-27 20:27:47 +0200

    Fixed rev_wstr

--- lvl3/rev_wstr/rev_wstr.c
+++ lvl3/rev_wstr/rev_wstr.c
@@ -1,85 +1,92 @@
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-int	count_words(char *str)
+int	word_count(char *str)
 {
-	int	i = 0;
-	int	p = 0;
-	int	c = 0;
+	int		i = 0;
+	int		w = 0;
+	bool	p = 1;
 
 	while (str[i])
 	{
-		if (str[i] != '\t' && str[i] != ' ' && str[i] != '\0')
+		if (str[i] != ' ' && str[i] != '\t' && p)
 		{
-			if (p == 0)
-			{
-				p = 1;
-				c++;
-			}
+			w++;
+			p = false;
 		}
-		else if (p == 1)
-			p = 0;
+		else if (str[i] == ' ' || str[i] == '\t')
+			p = true;
 		i++;
 	}
-	return (c);
+	return (w);
 }
 
-int	ft_strlen(char *str)
+char	*fill_word(int start, int len, char *str)
 {
-	int i = 0;
+	char	*word;
+	int		i = 0;
 
-	while (str[i])
-		i++;
-	return (i);
-}
-
-char	*fill_words(char *src, char *dst, int start, int total)
-{
-	int	i = 0;
-
-	while ((start + i) < total && src[start + i])
+	word = malloc(sizeof(char) * (len + 1));
+	while (i < len)
 	{
-		dst[i] = src[start + i];
+		word[i] = str[start + i];
 		i++;
 	}
-	dst[i] = '\0';
-	return (dst);
+	word[i] = '\0';
+	return (word);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i = -1;
+
+	while (str[++i])
+		write(1, &str[i], 1);
 }
 
 void	rev_wstr(char *str)
 {
 	char	**array;
-	int		i;
-	int		j = 0;
+	int		i = 0;
+	int		j;
 	int		k = 0;
 
-	array = malloc(sizeof(char *) * (count_words(str) + 1));
+	k = word_count(str);
+	array = malloc(sizeof(char *) * (k + 1));
 	if (!array)
-		exit(1);
-	i = ft_strlen(str) - 1;
-	while (str[i])
 	{
-		while (str[i] != '\0' && (str[i] == '\t' || str[i] == ' '))
-			i--;
-		while (str[i - j] != '\t' && str[i - j] != ' ')
-			j++;
-		array[k] = malloc(sizeof(char) * (j + 1));
-		if (!array[k])
-			exit(1);
-		array[k] = fill_words(str, array[k], j, i);
-		i -= j;
-		k++;
+		free(array);
+		exit (1);
+	}
+	k = 0;
+	while (str[i] != '\0')
+	{
+		j = 0;
+		if (str[i] != ' ' && str[i] != '\t' && str[i] != '\0')
+		{
+			while (str[i + j] != ' ' && str[i + j] != '\t' && str[i + j])
+				j++;
+			array[k] = fill_word(i, j, str);
+			k++;
+			i += j;
+		}
+		else
+			i++;
 	}
 	array[k] = NULL;
-	k = -1;
-	while (array[++k] != NULL)
-		write(1, &array[k], ft_strlen(array[k]));
+	while (array[--k])
+	{
+		ft_putstr(array[k]);
+		if (array[k - 1] != NULL)
+			write(1, " ", 1);
+	}
 }
 
 int	main(int ac, char **av)
 {
 	if (ac == 2)
 		rev_wstr(av[1]);
-	write(1,"\n", 1);
+	write(1, "\n", 1);
 	return (0);
 }
